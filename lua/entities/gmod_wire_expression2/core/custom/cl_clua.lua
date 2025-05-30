c19190f863ee991d025196817761b9789141ccf4
bjurd <177191469+bjurd@users.noreply.github.com>
2025-05-30 01:48:32 -0400

    Better English

--- lua/entities/gmod_wire_expression2/core/custom/cl_clua.lua
+++ lua/entities/gmod_wire_expression2/core/custom/cl_clua.lua
@@ -1,2 +1,2 @@
 E2Helper.Descriptions["sendLuaString(s)"] = "Sends a Lua string via Player:SendLua() and is limited by its maximum 255 character limit."
-E2Helper.Descriptions["sendLongLuaString(s)"] = "Sends a Lua string via the net library and is limited to a 65535 character limit."
+E2Helper.Descriptions["sendLongLuaString(s)"] = "Sends a Lua string via the net library and has a 65535 character limit."
